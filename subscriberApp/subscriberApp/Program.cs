05eb81f876888047790e8b2ef5cf4fb5c1953e1e
mmh <moonmh0201@gmail.com>
2021-12-16 17:52:10 +0900

    HTML, CSS, TS add

--- subscriberApp/subscriberApp/Program.cs
+++ subscriberApp/subscriberApp/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
---
+++ subscriberApp/subscriberApp/wwwroot/scripts/counter-app.js
@@ -0,0 +1,2 @@
+console.log("첫번째 테스트. 구독자 카운팅 앱");
+//# sourceMappingURL=counter-app.js.map
